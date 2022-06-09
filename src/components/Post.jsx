835195d797fe5e36ab02a0d69a0a6377f7bccbd3
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-09 00:10:41 -0300

    :sparkles: feat(frontend): create post structure

---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/gbdsantos.png" />
+          <div className={styles.authorInfo}>
+            <strong>Guilherme Bezerra</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+
+        <time dateTime="2022-06-09 00:04:30" title="Nove de Maio às 00:05">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala pessoal 👋</p>
+        <p>Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻 </p>
+        <p>Acesse e deixe seu feedback 👉 <a href="" >devonlane.design</a></p>
+        <p><a href="">#uiux #userexperience</a></p>
+      </div>
+    </article>
+  );
+}
