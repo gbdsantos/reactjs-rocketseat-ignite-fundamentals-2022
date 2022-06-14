52f8faf00d12ed4a0db09209ccaa7a00afe1fe63
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-13 23:05:18 -0300

    :art: style(frontend): create comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,6 +26,18 @@ export function Post() {
           <a href="">#userexperience</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
