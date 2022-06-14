4a04be058b6b88e3896a0ca717efe09da87bc716
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-13 23:29:05 -0300

    :art: style(frontend): create Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/21063343?v=4" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Bezerra</strong>
+              <time dateTime="2022-06-09 00:04:30" title="Nove de Maio às 00:05">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Apagar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
