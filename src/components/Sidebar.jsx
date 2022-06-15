00eed385b5d9e78bbaf6da36308aa3684b7dc8ad
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-06-15 00:37:10 -0300

    :sparkles: feat(frontend): Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/21063343?v=4" />
+        <Avatar src="https://github.com/gbdsantos.png" />
         <strong>Guilherme Bezerra</strong>
         <span>Software Engineer</span>
       </div>
